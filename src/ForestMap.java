f9fa0131634e11c9f00b6c9752aa05fecc7c1a01
Martins Greizis <grei0003@hz.nl>
2023-01-19 17:52:34 +0100

    new paths in ForestMap

--- src/ForestMap.java
+++ src/ForestMap.java
@@ -2,24 +2,26 @@ import java.util.Scanner;
 
 class ForestMap implements Map {
     // Storing the locations in an array, so that adding new locations is easy
-    private final String[] locations = {"lake"};
+    private final String[] locations = {"lake", "cave"};
     public void display() {
         System.out.println("Locations in the forest: ");
         for (String location : locations) {
             System.out.println("- " + location);
         }
     }
+
     public void explore() {
         System.out.println("You start to explore the forest.");
-        System.out.println("The path begins to become darker and darker. Are you sure you want to continue? (yes, no)");
+        System.out.println("You come across a fork in the road. Which way would you like to go? (left, right)");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        if (input.equals("yes")) {
-            System.out.println("You keep walking the path and come across a beautiful lake.");
-        } else if (input.equals("no")) {
-            System.out.println("You decide to turn around and let the forest keep its secrets.");
+        if (input.equals("left")) {
+            System.out.println("You follow the left path and come across a beautiful lake.");
+        } else if (input.equals("right")) {
+            System.out.println("You follow the right path and come across a dark and mysterious cave.");
         } else {
             System.out.println("Invalid command. Please try again.");
         }
     }
 }
+
